c4d73a530e371b7d0352c9c70e5e8d4edaa40b2d
1mache <miktor4925@gmail.com>
2025-08-10 15:34:19 +0300

    Updated previously created classes Now MenuOption has a getNextOption method for queueing next option and a way to ask if it closes the menu. MenuPage implement this while using smaller helper functions

--- src/sEmulator/console/MenuOption.java
+++ src/sEmulator/console/MenuOption.java
@@ -1,8 +1,25 @@
 package sEmulator.console;
 
 public interface MenuOption {
-    // what happens when option gets chosen
-    void choose();
-    // returns the name of the option
-    String getName();
+    /**
+     * what happens when an option is selected
+     */
+    void select();
+
+    /**
+     * @return name of the option to display in menu
+     */
+    String  getName();
+
+    /**
+     * if this option includes other options in it we can request next option
+     * @return next option to be loaded
+     */
+    MenuOption getNextOption();
+
+    /**
+     * @return true if this option's selection is supposed to result
+     *         in menu being closed
+     */
+    boolean closesMenu();
 }
--- src/sEmulator/console/MenuPage.java
+++ src/sEmulator/console/MenuPage.java
@@ -1,14 +1,15 @@
 package sEmulator.console;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MenuPage implements MenuOption {
-    private ArrayList<MenuOption> options;
-    private MenuOption            chosenOption;
+    private final ArrayList<MenuOption> options;
 
     private final String name;
     private final String prompt;
 
+    // public:
     public MenuPage(String name, String prompt) {
         this.name = name;
         this.prompt = prompt;
@@ -16,20 +17,8 @@ public class MenuPage implements MenuOption {
     }
 
     @Override
-    public void choose() {
+    public void select() {
         System.out.println(prompt);
-
-        if(!options.isEmpty()){
-            System.out.println();
-            System.out.println("Choose an option: " +
-                    "(type number 1-" + options.size() + ")" );
-
-            String optionFormat = "[%d].%s";
-            for(int i = 0; i < options.size(); i++)
-            {
-                System.out.printf(optionFormat, i+1, options.get(i).getName());
-            }
-        }
     }
 
     @Override
@@ -37,8 +26,50 @@ public class MenuPage implements MenuOption {
         return name;
     }
 
+    @Override
+    public boolean closesMenu() {
+        return false; // menu page doesn't close menu when proceed() is called
+    }
+
+    public MenuOption getNextOption() {
+        if(options.isEmpty())
+            return null; // this menu has no options
+
+        System.out.println();
+        System.out.println("Choose an option: " +
+                    "(type number 1-" + options.size() + ")" );
+
+        String optionFormat = "[%d].%s";
+        for(int i = 0; i < options.size(); i++)
+        {
+            System.out.printf(optionFormat + '\n', i+1, options.get(i).getName());
+        }
+
+        int optionId = askForId();
+        return options.get(optionId);
+    }
+
     public void addOption(MenuOption option)
     {
         options.add(option);
+    }
+
+    private int askForId(){
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int optionId = input-1; // user counts them from 1
+        while (!checkValidOptionId(optionId))
+        {
+            System.out.printf("Invalid option. Please try again.%n");
+            input = scanner.nextInt();
+            optionId = input-1;
+        }
+
+        return optionId;
+    }
+
+    private boolean checkValidOptionId(int optionId) {
+        // check positive and in bounds
+        return (optionId >= 0 && optionId < options.size());
     }
 }
