9b20b532fe8769e154ab42bfdaf9a1d5c599c7af
1mache <miktor4925@gmail.com>
2025-08-20 17:28:00 +0300

    made constants for variable chars in VariableType

--- Engine/src/engine/variable/VariableType.java
+++ Engine/src/engine/variable/VariableType.java
@@ -3,17 +3,21 @@ package engine.variable;
 public enum VariableType {
     INPUT{
         @Override
-        public String stringRepresentation() {return "x";}
+        public String stringRepresentation() {return Character.toString(INPUT_VARIABLE_CHAR);}
     },
     WORK{
         @Override
-        public String stringRepresentation() {return "z";}
+        public String stringRepresentation() {return Character.toString(WORK_VARIABLE_CHAR);}
     },
     RESULT{
         @Override
-        public String stringRepresentation() {return "y";}
+        public String stringRepresentation() {return Character.toString(RESULT_VARIABLE_CHAR);}
     }
     ;
 
     public abstract String stringRepresentation();
+
+    public static final Character INPUT_VARIABLE_CHAR = 'x';
+    public static final Character WORK_VARIABLE_CHAR = 'z';
+    public static final Character RESULT_VARIABLE_CHAR = 'y';
 }
