a0079a84b66b2f571a49cb8851241031b450e2c1
1mache <miktor4925@gmail.com>
2025-09-13 17:53:28 +0300

    Added variable table subcomponent

---
+++ GUI/src/gui/component/variable/table/VariableTableController.java
@@ -0,0 +1,53 @@
+package gui.component.variable.table;
+
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class VariableTableController implements Initializable {
+    private record VariableEntry(String name, Long value) {}
+
+    @FXML
+    private TableColumn<VariableEntry, String> nameColumn;
+
+    @FXML
+    private TableColumn<VariableEntry, Long> valueColumn;
+
+    @FXML
+    private TableView<VariableEntry> variableTable;
+
+    private final ObservableList<VariableEntry> variableEntries = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        variableTable.setPlaceholder(new Label("No data"));
+
+        nameColumn.setCellValueFactory(cellData ->
+                        new ReadOnlyStringWrapper(cellData.getValue().name())
+        );
+        valueColumn.setCellValueFactory(cellData ->
+                new ReadOnlyObjectWrapper<>(cellData.getValue().value())
+        );
+    }
+
+    public void clear() {
+        this.variableEntries.clear();
+        variableTable.setItems(this.variableEntries);
+    }
+
+    public void setVariableEntries(Map<String, Long> variableMap) {
+        this.variableEntries.clear();
+        variableMap.forEach((name, value) -> this.variableEntries.add(new VariableEntry(name, value)));
+        variableTable.setItems(this.variableEntries);
+    }
+}
