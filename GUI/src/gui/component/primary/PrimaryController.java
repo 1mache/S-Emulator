510cfdf61b62f4c1c6ab5ea65c64ef7c4eab54c9
1mache <miktor4925@gmail.com>
2025-10-03 04:08:01 +0300

    Added CSS styling

--- GUI/src/gui/component/primary/PrimaryController.java
+++ GUI/src/gui/component/primary/PrimaryController.java
@@ -21,7 +21,6 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.CacheHint;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
