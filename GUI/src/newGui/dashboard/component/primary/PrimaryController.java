d3f9e6e6842b44cda59fb12d60138ddeef258fb1
shaytoledo <shayto2@mat.ac.il>
2025-10-09 16:37:23 +0300

    start building the client gui

---
+++ GUI/src/newGui/dashboard/component/primary/PrimaryController.java
@@ -0,0 +1,15 @@
+package newGui.dashboard.component.primary;
+
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrimaryController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
