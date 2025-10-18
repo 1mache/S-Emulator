3be07d81f15f979fae786d902c6593d5d64bb5f0
shaytoledo <shayto2@mat.ac.il>
2025-10-18 11:21:27 +0300

    Edit creditRequest parameter

--- Client/src/requests/LoadCreditRequest.java
+++ Client/src/requests/LoadCreditRequest.java
@@ -1,26 +1,23 @@
 package requests;
 
+import okhttp3.HttpUrl;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import util.Constants;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import static util.Constants.GSON_INSTANCE;
 
 public class LoadCreditRequest {
     public static Request build(int creditAmount) {
 
-        Map<String, Object> jsonMap = new LinkedHashMap<>();
-        jsonMap.put("creditamount", creditAmount);
-        String json = GSON_INSTANCE.toJson(jsonMap);
+        HttpUrl url = HttpUrl.parse(Constants.ADD_CREDIT)
+                .newBuilder()
+                .addQueryParameter("creditamount", String.valueOf(creditAmount))
+                .addQueryParameter("age", "25")
+                .build();
 
-        RequestBody body = RequestBody.create(json, Constants.MEDIA_TYPE_JSON);
 
         return new Request.Builder()
-                .url(Constants.ADD_CREDIT)
-                .post(body)
+                .url(url)
+                .get()
                 .build();
     }
 }
