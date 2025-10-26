4e9344a58f1d03aac934396a96851e2a64e31ffe
shaytoledo <shayto2@mat.ac.il>
2025-10-26 17:52:05 +0200

    fix history chain

--- Client/src/util/Constants.java
+++ Client/src/util/Constants.java
@@ -6,10 +6,8 @@ import okhttp3.MediaType;
 public class Constants {
 
     // global constants
-    public final static String LINE_SEPARATOR = System.getProperty("line.separator");
     public final static String START_NAME = "<Anonymous>";
     public final static int REFRESH_RATE = 2;
-    public final static String CHAT_LINE_FORMATTING = "%tH:%tM:%tS | %.10s: %s%n";
 
     // fxml locations
     public final static String MAIN_PAGE_FXML_RESOURCE_LOCATION = "/newGui/pages/primary/mainClientApp.fxml";
